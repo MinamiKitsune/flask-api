92c932b68a40ab4204ae9e0cd0d64f5095f9de0a
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-10-19 02:56:43 +0200

    fixed spelling mistake

--- app/api/vaccine/__init__.py
+++ app/api/vaccine/__init__.py
@@ -3,10 +3,10 @@ from flask_restful import Api
 
 # Create the blueprints and the API
 vaccine_BP = Blueprint('vaccine', __name__, url_prefix='/vaccine')
-vile_BP = Blueprint('vile', __name__, url_prefix='/vile')
+vial_BP = Blueprint('vial', __name__, url_prefix='/vial')
 vaccine_BP.register_blueprint(vile_BP) # Register the blueprint to the parent blueprint
-vile_api = Api(vile_BP)
+vial_api = Api(vial_BP)
 vaccine_api = Api(vaccine_BP)
 
 from . import vaccine
-from . import vile
+from . import vial
--- app/api/vaccine/vial.py
+++ app/api/vaccine/vial.py
@@ -1,10 +1,10 @@
 from flask_restful import Resource
-from . import vile_api
+from . import vial_api
 
 # This class will handle all of the methods related to the viles of a vaccine
-class Vile(Resource):
+class Vial(Resource):
     def get(self):
-        return {"response" : "vile"}, 200
+        return {"response" : "vial"}, 200
 
 # Add the resource to the API
-vile_api.add_resource(Vile, "/")
+vial_api.add_resource(Vile, "/")
