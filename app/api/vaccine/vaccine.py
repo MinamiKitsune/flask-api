eb306a297c844887f14c05090d47d487025f6c22
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-06 14:59:33 +0200

    added test endpoints for vial and vaccine

--- app/api/vaccine/vaccine.py
+++ app/api/vaccine/vaccine.py
@@ -1,10 +1,15 @@
 from flask_restful import Resource
 from . import vaccine_api
+from ..databaseModels import Vaccine
+from app import db
 
 # This class will handle all of the methods related to the the vaccines
-class Vaccine(Resource):
+class VaccineResource(Resource):
     def get(self):
-        return {"response" : "vaccine"}, 200
+        new_vaccine = Vaccine(vaccine_name = "test", target_disease = "test", number_to_administer = 2, dosage_interval = 0)
+        db.session.add(new_vaccine)
+        db.session.commit()
+        return {"response" : "added vaccine"}, 200
 
 # Add the resource to the API
-vaccine_api.add_resource(Vaccine, '/')
+vaccine_api.add_resource(VaccineResource, '/')
--- app/api/vaccine/vial.py
+++ app/api/vaccine/vial.py
@@ -1,10 +1,15 @@
 from flask_restful import Resource
 from . import vial_api
+from ..databaseModels import Vial
+from app import db
 
 # This class will handle all of the methods related to the vials of a vaccine
-class Vial(Resource):
+class VialResource(Resource):
     def get(self):
-        return {"response" : "vial"}, 200
+        new_vial = Vial(id_vial = "11111", vaccine_id = 1)
+        db.session.add(new_vial)
+        db.session.commit()
+        return {"response" : "Vial added"}, 200
 
 # Add the resource to the API
-vial_api.add_resource(Vial, "/")
+vial_api.add_resource(VialResource, "/")
