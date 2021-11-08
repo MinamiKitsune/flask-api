6cfcf938c2d8377b6e609b49ca09c8752d97594f
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-08 13:40:58 +0200

    added citizen database model

--- app/api/databaseModels.py
+++ app/api/databaseModels.py
@@ -10,3 +10,14 @@ class Vaccine(db.Model):
 class Vial(db.Model):
     id_vial = db.Column(db.Text, primary_key = True)
     vaccine_id = db.Column(db.Integer, db.ForeignKey('vaccine.id_vaccine'), nullable = False)
+
+class Citizen(db.Model):
+   id_citizen = db.Column(db.Text, primary_key=True)
+   email = db.Column(db.Text, nullable=False)
+   name = db.Column(db.Text, nullable=False)
+   surname = db.Column(db.Text, nullable=False)
+   date_of_birth = db.Column(db.Date, nullable=False)
+   mobile_num = db.Column(db.Text, nullable=False)
+   medical_aid = db.Column(db.Text, nullable=True)
+   address = db.Column(db.Text, nullable=False )
+   parent_id = db.Column(db.Integer, db.ForeignKey('citizen.id_citizen'), nullable = True)
