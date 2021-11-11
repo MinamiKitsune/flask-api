72eb5b9e911a76438a83913a329d2e5a22c87dcd
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-11 16:21:43 +0200

    finished citizen

--- app/api/citizen/citizen.py
+++ app/api/citizen/citizen.py
@@ -1,117 +1,137 @@
 from flask_restful import Resource, reqparse, abort, fields, marshal_with
+from datetime import datetime
 from . import citizen_api
 from ..databaseModels import Citizen
-from .. import dataHandler
+from .. import dataHandler, smsHandler
 from app import db
 
-# TODO redo most of this to allow for better integration
-#Parser to check that required arguments are sent
-citizen_post_args = reqparse.RequestParser()
-citizen_post_args.add_argument("email", type=str, help="Email is required", required=True)
-citizen_post_args.add_argument("name", type=str, help="Name is required", required=True)
-citizen_post_args.add_argument("surname", type=str, help="Surname is required", required=True)
-citizen_post_args.add_argument("date_of_birth", type=str, help="Date of birth is required", required=True)
-citizen_post_args.add_argument("mobile_num", type=str, help="Mobile number is required", required=True)
-citizen_post_args.add_argument("medical_aid", type=str, required=False)
-citizen_post_args.add_argument("address", type=str, help="Address is required", required=True)
-citizen_post_args.add_argument("parent_id", type=int, required=False)
 
-#Parser to check that required arguments are sent to update
+# Parser to check that required arguments are sent to add citizen
 citizen_put_args = reqparse.RequestParser()
-citizen_put_args.add_argument("medical_aid", type=str)
+citizen_put_args.add_argument("id_citizen", type=str, help="The ID of the citizen in string format", required=True)
+citizen_put_args.add_argument("email", type=str, help="Email is required in string format", required=True)
+citizen_put_args.add_argument("name", type=str, help="Name is required in string format", required=True)
+citizen_put_args.add_argument("surname", type=str, help="Surname is required in string format", required=True)
+citizen_put_args.add_argument("date_of_birth", type=str, help="Date of birth is required in the format YYYY-MM-DD", required=True)
+citizen_put_args.add_argument("mobile_num", type=str, help="Mobile number is required in string format", required=True)
+citizen_put_args.add_argument("medical_aid", type=str, help="Medical aid number is required to be in string format")
+citizen_put_args.add_argument("citizen_address", type=str, help="Address is required in string format")
 
-#Fields to marshal the responses
+# Parser to check that required arguments are sent to get a citizen
+citizen_get_args = reqparse.RequestParser()
+citizen_get_args.add_argument("id_citizen", type=str, help="The ID of the citizen in string format", required=True)
+
+# Parser to check that required arguments are sent to update a citizen
+citizen_patch_args = reqparse.RequestParser()
+citizen_patch_args.add_argument("id_citizen", type=str, help="The ID of the citizen in string format", required=True)
+citizen_patch_args.add_argument("email", type=str, help="Email is required in string format")
+citizen_patch_args.add_argument("name", type=str, help="Name is required in string format")
+citizen_patch_args.add_argument("surname", type=str, help="Surname is required in string format")
+citizen_patch_args.add_argument("date_of_birth", type=str, help="Date of birth is required in the format YYYY-MM-DD")
+citizen_patch_args.add_argument("mobile_num", type=str, help="Mobile number is required in string format")
+citizen_patch_args.add_argument("medical_aid", type=str, help="Medical aid number is required to be in string format")
+citizen_patch_args.add_argument("citizen_address", type=str, help="Address is required in string format")
+
+# Parser to check that required arguments are sent to delete a citizen
+citizen_del_args = reqparse.RequestParser()
+citizen_del_args.add_argument("id_citizen", type=str, help="The ID of the citizen in string format", required=True)
+
+# Fields to marshal the responses
 resource_fields = {
-        'id_citizen': fields.Integer,
-        'email': fields.String,
-        'name': fields.String,
-        'surname': fields.String,
-        'date_of_birth': fields.String,
-        'medical_aid': fields.String,
-        'address': fields.String,
-        'parent_id': fields.Integer,
+    'id_citizen': fields.String,
+    'email': fields.String,
+    'name': fields.String,
+    'surname': fields.String,
+    'date_of_birth': fields.String,
+    'mobile_num': fields.String,
+    'medical_aid': fields.String,
+    'citizen_address': fields.String
 }
 
 #Class to handle methods related to citizens
 class CitizenResource(Resource):
-    #Function to retrieve existing citizen
     @marshal_with(resource_fields)
-    def get(self, citizen_id):
-        citizen = CitizenModel.query.filter_by(id_citizen = citizen_id).first()
+    def get(self):
+        args = citizen_get_args.parse_args()
+        dataHandler.cleanData(args)
+        return getCitizen(args), 200
 
-        if not citizen:
-           abort(404, message = "Citizen with this ID does not exist")
-        return citizen
-
-    #Function to create new citizen     
-    @marshal_with(resource_fields)
-    def post(self, citizen_id):
-        args = citizen_post_args.parse_args()
-        citizen = CitizenModel.query.filter_by(id_citizen=citizen_id).first()
-
-        if citizen:
-            abort(409, message = "Citizen ID Taken")
-        try:
-            citizenInfo = CitizenModel(
-                                id_citizen=citizen_id,
-                                email=args['email'],
-                                name=args['name'],
-                                surname=args['surname'],
-                                date_of_birth=args['date_of_birth'],
-                                mobile_num=args['mobile_num'],
-                                medical_aid=args['medical_aid'],
-                                address=args['address'],
-                                parent_id=args['parent_id']
-                            )
-            db.session.add(citizenInfo)
-            db.session.commit()
-            SMS()
-
-            return citizenInfo, 201
-
-        except Exception as e:
-            return 400  
-
-    #Function to update citizen medical aid 
-    @marshal_with(resource_fields)
-    def put(self, citizen_id):
+    def put(self):
         args = citizen_put_args.parse_args()
-        citizen = CitizenModel.query.filter_by(id_citizen=citizen_id).first()
+        dataHandler.cleanData(args)
+        addCitizen(args)
+        return { "message": "Added to database" }, 201
     
-        if not citizen:
-            abort(404, messaege = "Citizen with this ID does not exist, cannot update")
+    def patch(self):
+        args = citizen_patch_args.parse_args()
+        dataHandler.cleanData(args)
+        updateCitizen(args)
+        return { "message": "Updated the database" }, 200
+    
+    def delete(self):
+        args = citizen_del_args.parse_args()
+        deleteCitizen(args)
+        return { "message": "Deleted from database" }, 204
 
-        if args['medical_aid']:
-            citizen.medical_aid = args['medical_aid']
+# Add resource to the API
+citizen_api.add_resource(CitizenResource, "")
 
+# Get a citizen by their ID
+def getCitizen(args):
+    result = Citizen.query.filter_by(id_citizen=args["id_citizen"]).first()
+    if result:
+        return result
+    else:
+        abort(404, message="A citizen with this ID does not exist")
+
+# Add a citizen to the database
+def addCitizen(args):
+    result = Citizen.query.filter_by(id_citizen = args["id_citizen"]).first()
+    if result:
+        abort(409, message="A citizen with this ID already exists")
+    else:
+        try:
+            date = datetime.strptime(args["date_of_birth"], '%Y-%m-%d')
+        except ValueError:
+            abort(400, message="The date should be in format YYYY-MM-DD")
+        new_citizen = Citizen(id_citizen = args["id_citizen"], email = args["email"], name = args["name"],
+        surname = args["surname"], date_of_birth = date, mobile_num = args["mobile_num"], medical_aid = args["medical_aid"],
+        citizen_address = args["citizen_address"])
+        db.session.add(new_citizen)
+        db.session.commit()
+        smsHandler.sendMockSms("You have been added", args["mobile_num"])
+
+# Update the citizen in the database
+def updateCitizen(args):
+    result = Citizen.query.filter_by(id_citizen=args["id_citizen"]).first()
+    if not result:
+        abort(404, message="Citizen with this ID does not exist, cannot update")
+    else:
+        if args["email"]:
+            result.email = args["email"]
+        if args["name"]:
+            result.name = args["name"]
+        if args["surname"]:
+            result.surname = args["surname"]
+        if args["date_of_birth"]:
+            try:
+                date = datetime.strptime(args["date_of_birth"], '%Y-%m-%d')
+            except ValueError:
+                abort(400, message="The date should be in format YYYY-MM-DD")
+            result.date_of_birth = date
+        if args["mobile_num"]:
+            result.mobile_num = args["mobile_num"]
+        if args["medical_aid"]:
+            result.medical_aid = args["medical_aid"]
+        if args["citizen_address"]:
+            result.citizen_address = args["citizen_address"]
         db.session.commit()
 
-        return {"message": "Citizen updated"}, 200
-
-#Class to handle methods related to citizena
-class CitizensList(Resource):
-    #Fuction to retrieve all citizens
-    def get(self):
-        citizen = CitizenModel.query.all()
-        citizensList = {}
-
-        for user in citizen:
-            citizensList[user.id_citizen] = {"email": user.email,
-                                                "name":user.name,
-                                                "surname":user.surname,
-                                                "date_of_birth": user.date_of_birth,
-                                                "mobile_num": user.mobile_num,
-                                                "medical_aid": user.medical_aid,
-                                                "address": user.address,
-                                                "parent_id": user.parent_id} 
-        return citizensList
-
-#add resource to the API
-#dependent resource
-citizen_api.add_resource(CitizenResource, "/citizens/<int:citizen_id>")
-#dependent list resource
-citizen_api.add_resource(CitizensList, "/citizens")  
-
-#Function to send sms notification 
-def SMS(): 
-    print("An sms will be sent to you shortly...")  
+# delete from the database
+def deleteCitizen(args):
+    result = Citizen.query.filter_by(id_citizen=args["id_citizen"]).first()
+    if not result:
+        abort(404, message="Citizen with this ID does not exist, cannot delete")
+    else:
+        db.session.delete(result)
+        db.session.commit()
