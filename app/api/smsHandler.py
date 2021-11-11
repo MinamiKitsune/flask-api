8c70c91e8a87c4a49157fa2caa38442f7e30434c
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-11 17:32:37 +0200

    added a mock smsHandler

--- app/api/smsHandler.py
+++ app/api/smsHandler.py
@@ -1,1 +1,6 @@
 # TODO add code to allow for sms to be sent
+def sendMockSms(message, number):
+    print(message + " to the number: " + number)
+
+def sendMockVacinationSms(message, number, uniqueID):
+    print(message + " to the number: " + number + " and their unique ID is: " + uniqueID)
