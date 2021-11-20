08bc9ba73c2b2e1ac29e7b21ed744ae9b898b05c
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-21 00:27:45 +0200

    created token_required decorator

---
+++ app/api/decorator.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from flask import request
+import jwt
+from .. import app
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return {'message': 'A Token has not been provided, please login and collect a token'}, 401
+        try:
+            jwt.decode(token, app.config['SECRET_KEY'])
+        except Exception as e:
+            return {'message': 'Token is invalid!'}, 401
+        return f(*args, **kwargs)
+
+    return decorated
