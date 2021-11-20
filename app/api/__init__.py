948ed9e991e13ac46818d5e71251087a8175f136
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-21 00:31:13 +0200

    added user blueprints and api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,5 +4,8 @@ from flask_restful import Api
 # Create the blueprint and the API
 test_bp = Blueprint('test', __name__, url_prefix='/test')
 test_api = Api(test_bp)
+user_bp = Blueprint('user', __name__, url_prefix='/user')
+user_api = Api(user_bp)
 
 from . import test_conn
+from . import user
