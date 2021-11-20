cc3b0c7b3d7f6502f5442f84f8e39d6c75462eab
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-21 00:28:20 +0200

    added more test endpoints

--- app/api/test_conn.py
+++ app/api/test_conn.py
@@ -1,5 +1,8 @@
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, abort
 from . import test_api
+from .decorator import token_required
+from . import data_handler
 
 
 # This class is used to test the connection to the API
@@ -8,5 +11,22 @@ class TestConnection(Resource):
         return {"response": "successful"}, 200
 
 
+class TestAuthorisation(Resource):
+    @token_required
+    def get(self):
+        return {"response": "successful"}, 200
+
+
+class TestAdmin(Resource):
+    @token_required
+    def get(self):
+        if data_handler.check_if_admin(request.headers['x-access-token']):
+            return {"response": "successful"}, 200
+        else:
+            abort(403, message="Forbidden.")
+
+
 # Add the resources to the API
 test_api.add_resource(TestConnection, '/')
+test_api.add_resource(TestAuthorisation, '/auth')
+test_api.add_resource(TestAdmin, '/admin')
