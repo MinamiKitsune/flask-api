40276585a6382fdfe20b75bd758fc88486e066e2
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-21 00:29:15 +0200

    added check for admin

--- app/api/data_handler.py
+++ app/api/data_handler.py
@@ -1,3 +1,8 @@
+import jwt
+from .database_models import User
+from .. import app
+
+
 # Check if the arguments are empty
 def check_if_empty(args):
     for x in args:
@@ -26,3 +31,12 @@ def remove_upper(args):
     for x in args:
         if isinstance(args[x], str):
             args[x] = str(args[x]).lower()
+
+
+def check_if_admin(token):
+    data = jwt.decode(token, app.config['SECRET_KEY'])
+    result = User.query.filter_by(public_id=data["public_id"]).first()
+    if result.admin:
+        return True
+    else:
+        return False
