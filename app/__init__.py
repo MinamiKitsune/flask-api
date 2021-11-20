77607abc031dad139749dfc692f5cc5c380d653a
Ruan Swanepoel <ruan.swanepoel@outlook.com>
2021-11-21 00:29:52 +0200

    added secret key and blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,24 @@ from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 db = None
+app = None
 
 
 def create_app():
     # Create the flask application
+    global app
     app = Flask(__name__)
 
     # Configure the database
     app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///vaccine.db'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['SECRET_KEY'] = 'Do-not-tempt-me-Frodo'
     global db
     db = SQLAlchemy(app)
 
     # Import all of the blueprints
     from .api import test_bp
+    from .api import user_bp
     from .api.vaccine import vaccine_bp
     from .api.vaccination import vaccination_bp
     from .api.location import location_bp
@@ -26,6 +30,7 @@ def create_app():
 
     # Register all of the blueprints to the parent
     api_bp.register_blueprint(test_bp)
+    api_bp.register_blueprint(user_bp)
     api_bp.register_blueprint(vaccine_bp)
     api_bp.register_blueprint(vaccination_bp)
     api_bp.register_blueprint(location_bp)
